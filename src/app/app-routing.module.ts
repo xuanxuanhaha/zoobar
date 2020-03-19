963f9bbecc80f08541e4c8abccb3a0adb7335281
Sarveshwaran Rajarajan <sarvesh.ancc@gmail.com>
2020-03-20 00:54:32 +1100

    initial commit message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
 import {FormsModule} from '@angular/forms';
 
 
 const routes: Routes = [
-  {path: 'about', component: AboutComponent}
-]
+  {path: '', component: AboutComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent}
+];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
