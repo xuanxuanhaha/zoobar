e320bd5b1001ee73697a1d19ad8d36d984df827a
Sarveshwaran Rajarajan <sarvesh.ancc@gmail.com>
2020-03-21 00:53:42 +1100

    contact page - ramping up with the final design - without black filter in the bg

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  customerName: any = '';
+  customerEmail: any = '';
+  customerMobile: any = '';
+  customerSubject: any = '';
+  customerMessage: any = '';
+
+  nameShow: any = false;
+  emailShow: any = false;
+  mobileShow: any = false;
+  invalidEmail: any = false;
+  enquiryMessageShow: any = false;
+  bigErrorShow: any = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  scrollDown() {
+    const idSpace = document.getElementById('enquiry_section');
+    idSpace.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest' });
   }
 
 }
