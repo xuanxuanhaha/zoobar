9a86cb3c0f3cfe8f1c8cb9fb75777f8c898fd78e
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-03-20 19:01:52 +1100

    need to justify

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -22,6 +22,9 @@ export class AboutComponent implements OnInit {
         if(window.scrollY > screen.height / 2 && window.scrollY < screen.height - 100){
           document.getElementById('fixed-top').style.display = 'none';
           document.getElementById('fixed-top2').style.display = 'inline';
+          document.getElementById('fixed-top3').style.display = 'inline';
+          document.getElementById('fixed-top4').style.display = 'inline';
+          document.getElementById('fixed-top5').style.display = 'inline';
           console.log(window.scrollY);
           window.scroll({
             top: screen.height,
@@ -33,6 +36,9 @@ export class AboutComponent implements OnInit {
         } else{
           document.getElementById('fixed-top').style.display = 'inline';
           document.getElementById('fixed-top2').style.display = 'none';
+          document.getElementById('fixed-top3').style.display = 'none';
+          document.getElementById('fixed-top4').style.display = 'none';
+          document.getElementById('fixed-top5').style.display = 'none';
 
         }
 
@@ -42,9 +48,15 @@ export class AboutComponent implements OnInit {
         if(window.scrollY > screen.height / 2){
           document.getElementById('fixed-top').style.display = 'none';
           document.getElementById('fixed-top2').style.display = 'inline';
+          document.getElementById('fixed-top3').style.display = 'inline';
+          document.getElementById('fixed-top4').style.display = 'inline';
+          document.getElementById('fixed-top5').style.display = 'inline';
         }else{
           document.getElementById('fixed-top').style.display = 'inline';
           document.getElementById('fixed-top2').style.display = 'none';
+          document.getElementById('fixed-top3').style.display = 'none';
+          document.getElementById('fixed-top4').style.display = 'none';
+          document.getElementById('fixed-top5').style.display = 'none';
           console.log(window.scrollY);
           window.scroll({
             top: 0,
@@ -67,9 +79,15 @@ export class AboutComponent implements OnInit {
   ngOnInit() {
     window.scrollTo(0, 0);
     console.log(screen.height);
+    const b = screen.height;
     const a = screen.height.toString();
     document.getElementById('first-image-background').style.height = a + 'px';
     document.getElementById('second-image-background').style.height = a + 'px';
+
+
+    document.getElementById('fixed-top4').style.marginTop = (Number(a) - 450) + 'px';
+
+    document.getElementById('fixed-top5').style.marginTop = (Number(a) - 550) + 'px';
 
   }
 
