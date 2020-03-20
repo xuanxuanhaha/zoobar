86d3f3fb72d2d20e99dfb6156d17f7300401cd9f
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-03-20 17:40:40 +1100

    change hidden or show of the trasparent box in about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, NgZone, OnInit} from '@angular/core';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,60 @@ import {Component, HostListener, OnInit} from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  lastScrollTop = 0;
+  direction = '';
+
+  constructor(lc: NgZone) {
+    window.onscroll = () => {
+      const st = window.pageYOffset;
+      let dir = '';
+      if (st > this.lastScrollTop) {
+        dir = 'down';
+        console.log('down');
+        // 向上拉，如果到了第一张图的一半，就自己上去
+        if(window.scrollY > screen.height / 2 && window.scrollY < screen.height - 100){
+          document.getElementById('fixed-top').style.display = 'none';
+          document.getElementById('fixed-top2').style.display = 'inline';
+          console.log(window.scrollY);
+          window.scroll({
+            top: screen.height,
+            left: 0,
+            behavior: 'auto'
+          });
+        } else if(window.scrollY > screen.height - 100){
+
+        } else{
+          document.getElementById('fixed-top').style.display = 'inline';
+          document.getElementById('fixed-top2').style.display = 'none';
+
+        }
+
+      } else {
+        dir = 'up';
+        console.log('up');
+        if(window.scrollY > screen.height / 2){
+          document.getElementById('fixed-top').style.display = 'none';
+          document.getElementById('fixed-top2').style.display = 'inline';
+        }else{
+          document.getElementById('fixed-top').style.display = 'inline';
+          document.getElementById('fixed-top2').style.display = 'none';
+          console.log(window.scrollY);
+          window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'auto'
+          });
+
+        }
+      }
+      this.lastScrollTop = st;
+      lc.run(() => {
+        this.direction = dir;
+      });
+
+
+    };
+  }
 
 
   ngOnInit() {
@@ -15,6 +69,8 @@ export class AboutComponent implements OnInit {
     console.log(screen.height);
     const a = screen.height.toString();
     document.getElementById('first-image-background').style.height = a + 'px';
+    document.getElementById('second-image-background').style.height = a + 'px';
+
   }
 
 }
